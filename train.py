46438e12352f0bfbc037ed47b85beeb9d000e981
Tansil011019 <wilsontansil@gmail.com>
2025-11-12 12:16:48 -0600

    feat: add train experiment pipeline

--- train.py
+++ train.py
@@ -1,41 +1,90 @@
 import hydra
 from omegaconf import DictConfig, OmegaConf
 import logging
+from hydra.utils import instantiate
+import pandas as pd
+from torch.utils.data import Subset
+import torch
+from base import Trainer
+import os
 
+os.environ["CUDA_VISIBLE_DEVICES"] = "1, 2, 3"
 logger = logging.getLogger(__name__)
+
+batch_scheduler = ["OneCycleLR"]
 
 @hydra.main(version_base=None, config_path="config", config_name="config")
 def train(cfg: DictConfig) -> None:
     logger.info("Configuration: ")
     logger.info(f"\t{OmegaConf.to_yaml(cfg)}") 
 
-    logger.info("Phase 1: Dataloader")
-    dataloader_config = cfg.dataloader
-    train_loader = hydra.utils.instantiate(dataloader_config.train_loader)
-    val_loader = hydra.utils.instantiate(dataloader_config.val_loader)
-    logger.info("Checking DataLoaders...")
-    try:
-        # 1. Check training loader
-        logger.info(f"Train dataset size: {len(train_loader.dataset)}")
-        images, labels = next(iter(train_loader))
-        logger.info("Train loader check PASS. Batch details:")
-        logger.info(f"  Images shape: {images.shape}, dtype: {images.dtype}")
-        logger.info(f"  Labels shape: {len(labels)}")
-        logger.info(f"  Images min: {images.min():.2f}, max: {images.max():.2f}")
+    torch.manual_seed(cfg.seed)
 
-        # 2. Check validation loader
-        logger.info(f"Val dataset size: {len(val_loader.dataset)}")
-        images, labels = next(iter(val_loader))
-        logger.info("Val loader check PASS. Batch details:")
-        logger.info(f"  Images shape: {images.shape}, dtype: {images.dtype}")
-        logger.info(f"  Labels shape: {len(labels)}")
-        logger.info(f"  Images min: {images.min():.2f}, max: {images.max():.2f}")        
-    except Exception as e:
-        logger.error(f"Error loading data: {e}", exc_info=True)
-        return 
-    logger.info("Dataloader Loaded")
+    df = pd.read_csv(cfg.paths.csv_file)
+    unique_labels = df['label'].unique()
+    label_mapping = {label: idx for idx, label in enumerate(unique_labels)}
 
-    logger.info("Phase 2: Training")
+    train_dataset = instantiate(cfg.dataset.train_dataset, label_map=label_mapping)
+    val_dataset = instantiate(cfg.dataset.val_dataset, label_map=label_mapping)
+
+    histories = []
+    skf = instantiate(cfg.strategy)
+    for fold_index, (train_indices, val_indices) in enumerate(skf.split(df, df['label'].values)):
+        logger.info(f"Fold {fold_index + 1}/{cfg.strategy.n_splits}")
+        
+        train_subset = Subset(train_dataset, train_indices)
+        val_subset = Subset(val_dataset, val_indices)
+
+        train_loader = instantiate(cfg.dataloader.train_loader, dataset=train_subset)
+        val_loader = instantiate(cfg.dataloader.val_loader, dataset=val_subset)
+        
+        device = torch.device(cfg.model.training.device)
+        model = instantiate(cfg.model.params)
+        model = model.to(device)
+
+        optimizer = instantiate(cfg.optimizer, params=model.parameters())
+        criterion = instantiate(cfg.loss)
+
+        scheduler = None
+        scheduler_step_at_epoch_end = True
+
+        if cfg.scheduler._target_ is not None:
+            if batch_scheduler.__contains__(cfg.scheduler._target_.split('.')[-1]):
+                scheduler = instantiate(
+                    cfg.scheduler, 
+                    optimizer=optimizer, 
+                    steps_per_epoch=len(train_loader),
+                    epochs=cfg.model.traning.epochs
+                )
+                scheduler_step_at_epoch_end = False
+            else:
+                scheduler = instantiate(cfg.scheduler, optimizer=optimizer)
+
+        trainer = Trainer(
+            model=model,
+            model_name=cfg.model.name,
+            optimizer=optimizer,
+            criterion=criterion,
+            train_loader=train_loader,
+            val_loader=val_loader,
+            device=device,
+            epoch=cfg.model.training.epochs,
+            scheduler=scheduler,
+            early_stopping=cfg.model.training.early_stopping,
+            patience=cfg.model.training.patience,
+            min_delta=cfg.model.training.min_delta,
+            save_path=f"{cfg.paths.results_dir}",
+            fold_index=fold_index,
+            scheduler_step_at_epoch_end=scheduler_step_at_epoch_end
+        )
+
+        history = trainer.run()
+        histories.append(history)
+
+        os.makedirs(cfg.paths.results_dir, exist_ok=True)
+
+    with open(f"{cfg.paths.results_dir}/{cfg.model.name}/history_{cfg.model.name}.csv", 'w') as f:
+        pd.DataFrame(histories).to_csv(f, index=False)
     
     logger.info("Training Complete")   
 

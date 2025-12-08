1d37fccbf7de082fb75794e2f4dae731cca17870
Tansil011019 <wilsontansil@gmail.com>
2025-12-08 19:13:16 +0000

    feat add base splitter

--- base/__init__.py
+++ base/__init__.py
@@ -1,5 +1,6 @@
 from .validator import Validator
 from .trainer import Trainer
 from .rgan import RGANGenerator, RGANDiscriminator
+from .splitter import BaseSplitter
 
-__all__ = [Trainer, Validator, RGANGenerator, RGANDiscriminator]
+__all__ = [Trainer, Validator, RGANGenerator, RGANDiscriminator, BaseSplitter]
